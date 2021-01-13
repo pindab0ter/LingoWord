6a71db1de3a9a01ca51bbbc48aa9ad4cefbdfec6
pindab0ter <hansvanluttikhuizen@me.com>
2021-01-13 21:39:20 +0100

    Update colors and add app icon

--- Shared/View/LetterView.swift
+++ Shared/View/LetterView.swift
@@ -14,17 +14,16 @@ struct LetterView: View {
         GeometryReader { geometry in
             ZStack {
                 RoundedRectangle(cornerRadius: radius(for: geometry.size))
-                    .fill(Color.blue)
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2666666667, green: 0.5882352941, blue: 0.8352941176, alpha: 1)), Color(#colorLiteral(red: 0.06666666667, green: 0.4117647059, blue: 0.7647058824, alpha: 1))]), startPoint: .top, endPoint: .bottom))
                     .shadow(radius: radius(for: geometry.size))
                 
                 if letter.status == .unplaced {
                     Circle()
-                        .fill(Color.yellow)
+                        .fill(Color(#colorLiteral(red: 0.862745098, green: 0.7254901961, blue: 0.3137254902, alpha: 1)))
                 } else if letter.status == .placed {
                     RoundedRectangle(cornerRadius: radius(for: geometry.size))
-                        .fill(Color.red)
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9176470588, green: 0.2235294118, blue: 0.2784313725, alpha: 1)), Color(#colorLiteral(red: 0.9058823529, green: 0.1215686275, blue: 0.1882352941, alpha: 1)), Color(#colorLiteral(red: 0.9058823529, green: 0.1215686275, blue: 0.1882352941, alpha: 1))]), startPoint: .top, endPoint: .bottom))
                 }
-
                 Text(String(letter.character?.uppercased() ?? "."))
                     .shadow(radius: radius(for: geometry.size))
             }
